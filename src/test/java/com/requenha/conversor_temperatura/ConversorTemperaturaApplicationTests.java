45f3ef4b5e94d3dc5fa15efd22e1c91311c09886
Giovanni <142685364+Rivanns@users.noreply.github.com>
2026-09-04 21:26:10 -0300

    Initial commit

---
+++ src/test/java/com/requenha/conversor_temperatura/ConversorTemperaturaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.requenha.conversor_temperatura;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConversorTemperaturaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
