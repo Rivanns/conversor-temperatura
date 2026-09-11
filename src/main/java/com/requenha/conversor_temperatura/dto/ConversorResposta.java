654b1b3c86a7f71f9916762ac0e4fdab2d410e20
Giovanni <142685364+Rivanns@users.noreply.github.com>
2026-09-11 16:50:28 -0300

    implementa response
    adiciona getters & setters

--- src/main/java/com/requenha/conversor_temperatura/dto/ConversorResposta.java
+++ src/main/java/com/requenha/conversor_temperatura/dto/ConversorResposta.java
@@ -1,5 +1,35 @@
 package com.requenha.conversor_temperatura.dto;
 
 public class ConversorResposta {
+	private double temperatura;
+	private String unidade;
+	
+	
+	public ConversorResposta(double resposta, String unidade) {
+		this.temperatura = temperatura;
+		this.unidade = unidade;
+	}
+
+
+	public double getTemperatura() {
+		return temperatura;
+	}
+
+
+	public void setTemperatura(double temperatura) {
+		this.temperatura = temperatura;
+	}
+
+
+	public String getUnidade() {
+		return unidade;
+	}
+
+
+	public void setUnidade(String unidade) {
+		this.unidade = unidade;
+	}
+	
+	
 
 }
